8d5236c108e0694fa472709cb6c008cdda382c1c
pfittante <pfittante@yahoo.com>
2017-07-01 02:49:28 -0400

    Entered exit statements process section Project Commit

--- ProjectOne/Program.cs
+++ ProjectOne/Program.cs
@@ -18,8 +18,24 @@ namespace ProjectOne
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)        
         {
+            //Global Variables
+            string readAnserUp;
+            string firstUpper;
+            string lastUpper;
+            string birthMonthUpper;
+            string roybivChoiceUpper;
+            string userContUpper;
+            string vacationHome;
+            string autoChoice;
+            int custAge;            
+            int birthMonth1;            
+            int numSiblings;            
+            int yearRetire;            
+            double bankAcctFuture;
+
+
             //This first section will solicit buisness from the user
 
             Console.Write("\t\t\tWelcome I am Madamme Zelgas  \n\n\n");
@@ -27,68 +43,131 @@ namespace ProjectOne
             Console.Write("\t\tWould you like Madame Zelga to read your future?\n\n\n");
 
             Console.Write("\t\t\tPlease enter \"Yes\" or \"Quit\" :");
+
             string readAnsw = Console.ReadLine();
-            string readAnserUp = readAnsw.ToUpper();
+
+            readAnserUp = readAnsw.ToUpper();
 
             if (readAnserUp == "QUIT")
             {
-                Console.WriteLine("\n\n\t\t\t    Nobody likes a quitter!!");
+                Console.WriteLine("\n\n\t\t\t    Nobody likes a quitter!!\n\n\n");
+
+                Console.WriteLine("\n\n\t\t\t             Au Revoir!!\n\n\n");
+
                 return;
             }
             else if (readAnserUp == "YES")
             {
                 Console.WriteLine("\n\n\t\tI see you are a brave soul, so lets get started.\n\n");
+
+                Console.WriteLine("\n\n\t\tYou can type \"Quit\" anytime if you get scared\n\n");
+            }
+            else
+            {
+                Console.WriteLine("\n\n\tMadame Zelga is worried you might not be able to read directions");
+
+                Console.Write("\n\n\t\t\tPlease enter \"Yes\" or \"Quit\" :");
+
+                readAnsw = Console.ReadLine();
+
+                readAnserUp = readAnsw.ToUpper();
+
             }
 
-            //This nesxt section will solicit the required information from the user
+            //This nesxt section will solicit the first and last name of the customer
+
             Console.WriteLine("********************************************************************************\n");
+
             Console.Write("Please give Zelga your first name: ");
+
             string firstName = Console.ReadLine();
-            string firstUpper = firstName.ToUpper();
+
+            firstUpper = firstName.ToUpper();
+
+            if (firstUpper == "QUIT")
+            {
+                Console.WriteLine("\n\n\t\t\t    Nobody likes a quitter!!\n");
+
+                Console.WriteLine("\n\n\t\t\t             Au Revoir!!\n\n\n");
+
+                return;
+            }
 
             if (firstUpper == "PETE" || firstUpper == "PETER")
+
                 Console.WriteLine("\n\nSuch a wonderful first nemae you have!!");
 
             Console.Write("\n\nPlease give Zelga your last name: ");
+
             string lastName = Console.ReadLine();
-            string lastUpper = lastName.ToUpper();
+
+            lastUpper = lastName.ToUpper();
+
+            if (lastUpper == "QUIT")
+            {
+                Console.WriteLine("\n\n\t\t\t    Nobody likes a quitter!!\n");
+
+                Console.WriteLine("\n\n\t\t\t             Au Revoir!!\n\n\n");
+
+                return;
+            }
 
             if (lastUpper == "FITTANTE")
-                Console.WriteLine("\n\nYour last name is magical!\n\n");
 
-            Console.Write("\n\nPlease give Zelga your age! I promise to keep it a secret! ");
-            int custAge = int.Parse(Console.ReadLine());
+                Console.Write("\n\nYour last name is magical!\n\n");
+
+
+            //This section will solicit the age of the customer
+
+            Console.Write("\n\n********************************************************************************\n");
+
+            Console.Write("\nPlease give Zelga your age! I promise to keep it a secret! ");
+
+            custAge = int.Parse(Console.ReadLine());
 
 
             if (custAge < 1)
             {
                 Console.WriteLine("\n\nYou are lying to Zelga\n\n");
+
                 Console.Write("\n\nPlease give Zelga your correct age! ");
+
                 custAge = int.Parse(Console.ReadLine());
             }
 
-            Console.Write("\n\nPlease tell Zelga the month you were born! ");
-            string birthMonth = Console.ReadLine();
-            string birthMonthUpper = birthMonth.ToUpper();
-            int birthMonth1;
+
+            //This section will solicit the month the customer was born
             
+            Console.Write("\n\nPlease tell Zelga the month you were born! ");
+
+            string birthMonth = Console.ReadLine();
+
+            birthMonthUpper = birthMonth.ToUpper();
+
             // This conditional statement is designed to ensure the correct user input. I know it is extremely long, and I hate using it. I would love some ideas
+
             if (birthMonthUpper != "JANUNARY" || birthMonthUpper != "FEBRUARY" || birthMonthUpper != "MARCH" || birthMonthUpper != "APRIL"
                 || birthMonthUpper != "MAY" || birthMonthUpper != "JUNE" || birthMonthUpper != "JULY" || birthMonthUpper != "AUGUST"
                 || birthMonthUpper != "SEPTEMBER" || birthMonthUpper != "OCTOBER" || birthMonthUpper != "NOVEMBER" || birthMonthUpper != "DECEMBER")
             {
+                //The body of this if statement was intentioanlly left blank
             }
-                else if (birthMonthUpper != "1" || birthMonthUpper != "2" || birthMonthUpper != "3" || birthMonthUpper != "4" || birthMonthUpper != "5"
-                || birthMonthUpper != "6" || birthMonthUpper != "7" || birthMonthUpper != "8" || birthMonthUpper != "9" || birthMonthUpper != "10"
-                || birthMonthUpper != "11" || birthMonthUpper != "12")
-                {
+            else if (birthMonthUpper != "1" || birthMonthUpper != "2" || birthMonthUpper != "3" || birthMonthUpper != "4" || birthMonthUpper != "5"
+            || birthMonthUpper != "6" || birthMonthUpper != "7" || birthMonthUpper != "8" || birthMonthUpper != "9" || birthMonthUpper != "10"
+            || birthMonthUpper != "11" || birthMonthUpper != "12")
+            {
                 Console.Write("\n\nZelga says please enter a correct month of the year\n");
+
                 Console.Write("\n\nPlease tell Zelga the month you were born! ");
+
                 birthMonth = Console.ReadLine();
+
                 birthMonthUpper = birthMonth.ToUpper();
-                }
-            
-           
+            }
+
+            //This switch statement will take the birthmonth as entered from the user 
+            //regardless of input type and convert into an int.
+
             switch (birthMonthUpper)
             {
                 case "JANUNARY":
@@ -132,27 +211,41 @@ namespace ProjectOne
                     birthMonth1 = int.Parse(birthMonth);
                     break;
             }
-            
-            //This section will assist the user in entering the correct favorite color
+
+            //This section will solicit a favorite color from the user
 
             Console.Write("\n\nPlease enter your favorite \"ROYGBIV\" color for Zelga!\n");
+
             Console.Write("\n\nIf you do not know what \"ROYGBIV\" is, please type \"Help\"");
+
             Console.Write("\n\n\nZelga will gladly help you! ");
+
             string roygbivChoice = Console.ReadLine();
-            string roybivChoiceUpper = roygbivChoice.ToUpper();
+
+            roybivChoiceUpper = roygbivChoice.ToUpper();
 
             if (roybivChoiceUpper == "HELP")
             {
                 Console.WriteLine("\n\nR = Red");
+
                 Console.WriteLine("O = Orange");
+
                 Console.WriteLine("Y = Yellow");
+
                 Console.WriteLine("G = Green");
+
                 Console.WriteLine("B = Blue");
+
                 Console.WriteLine("I = Indigo");
+
                 Console.WriteLine("V = Violet");
-                Console.WriteLine("\n\n\nNow that Madame Zelga helped you, please give"); 
+
+                Console.WriteLine("\n\n\nNow that Madame Zelga helped you, please give");
+
                 Console.Write("\n\nMadame Zelga your favorite \"ROYGBIV\" color! ");
+
                 roygbivChoice = Console.ReadLine();
+
                 roybivChoiceUpper = roygbivChoice.ToUpper();
             }
             else
@@ -160,129 +253,146 @@ namespace ProjectOne
                 Console.Write("\n\n{0} is Zelga's favorite as well!\n", roybivChoiceUpper);
             }
 
+            //This section will solicit the number of siblings from the user
+
             Console.WriteLine("\n\nOur journey is almost complete my dear!");
+
             Console.WriteLine("\n\nMadame Zelga has only one remaining question");
+
             Console.Write("\n\n\nMadame Zelga would like to know how my siblings you have! ");
-            int numSiblings = int.Parse(Console.ReadLine());
+
+            numSiblings = int.Parse(Console.ReadLine());
 
             if (numSiblings < 0)
             {
                 Console.WriteLine("Madame Zelga thinks your funny! You cannot enter a number less than zero!");
+
                 Console.Write("\n\n\nMadame Zelga would like to know how my siblings you have! ");
+
                 numSiblings = int.Parse(Console.ReadLine());
             }
 
             //This section will display the users fortune if they decide to continue
 
             Console.WriteLine("\n\n\nMadame Zelga is now ready to tell your fortune!");
-            Console.WriteLine("\n\nIf you want to see into your future, please type \"Yes\"");
-            Console.Write("\n\nIf you have lost you courage please typ \"Quit\" ");
-            string userCont = Console.ReadLine();
-            string userContUpper = userCont.ToUpper();
 
+            Console.WriteLine("\n\nIf you want to see into your future, please type \"Yes\"");
+
+            Console.Write("\n\nIf you have lost you courage please typ \"Quit\" ");
+
+            string userCont = Console.ReadLine();
+
+            userContUpper = userCont.ToUpper();
 
             if (userContUpper == "QUIT")
             {
                 Console.WriteLine("\n\n\t\t\t    Nobody likes a quitter!!");
+
                 return;
             }
             else
             {
                 Console.WriteLine("\n\nA brave soul you are indeed!");
-                               
-            }
 
-            //This section calulates the users fortune
+            }
 
             //This if statement calculates the retirement age
 
-            int yearRetire = 0;
+            yearRetire = 0;
 
             if (custAge % 2 == 0 && custAge >= 1 && custAge <= 25)
             {
-                yearRetire = 2045;
+                yearRetire = 35;
             }
             else if (custAge % 2 == 0 && custAge >= 26 && custAge <= 50)
             {
-                yearRetire = 2035;
+                yearRetire = 25;
             }
             else if (custAge % 2 == 0 && custAge >= 51 && custAge <= 80)
             {
-                yearRetire = 2025;
+                yearRetire = 15;
             }
             else if (custAge % 2 == 0 && custAge > 81)
             {
-                yearRetire = 2017;
+                yearRetire = 3;
             }
             else if (custAge >= 1 && custAge <= 25)
             {
-                yearRetire = 2055;
+                yearRetire = 25;
             }
             else if (custAge >= 26 && custAge <= 50)
             {
-                yearRetire = 2045;
+                yearRetire = 20;
             }
             else if (custAge >= 51 && custAge <= 80)
             {
-                yearRetire = 2035;
+                yearRetire = 15;
             }
             else if (custAge >= 81)
             {
-                yearRetire = 2018;
+                yearRetire = 2;
             }
 
-            //This switch statement will choose the vacation home for the user
-
-            string vacationHome;
+            //This switch statement will choose the vacation home location
+            
             switch (numSiblings)
             {
                 case 0:
                     vacationHome = "Amsterdam";
+
                     break;
                 case 1:
                     vacationHome = "London";
+
                     break;
                 case 2:
                     vacationHome = "Egypt";
+
                     break;
                 case 3:
                     vacationHome = "Prauge";
+
                     break;
                 default:
                     vacationHome = "Cleveland Texas";
+
                     break;
             }
 
-            //This switch statement will provide the future mode of transportation to the user
+            //This switch statement will choose the future mode of transportation 
 
-            string autoChoice;
             switch (roybivChoiceUpper)
             {
                 case "RED":
                     autoChoice = "BMW C250";
+
                     break;
                 case "ORANGE":
                     autoChoice = "1967 Ford Bronco";
+
                     break;
                 case "YELLOW":
                     autoChoice = "Ford Escape";
+
                     break;
                 case "BLUE":
                     autoChoice = "1967 Beetle";
+
                     break;
                 case "INDIGO":
                     autoChoice = "Harley RoadKing";
+
                     break;
                 default:
                     autoChoice = "VW Cabrolet";
+
                     break;
             }
 
 
             //This if else statement will process the users future bank account
-            //The user input does no allow for entering anyhting less than 1 or greater than 12
-
-           double bankAcctFuture = 0.00;
+            
+            bankAcctFuture = 0.00;
 
             if (birthMonth1 >= 1 && birthMonth1 <= 4)
             {
@@ -297,12 +407,14 @@ namespace ProjectOne
                 bankAcctFuture = 21000.00;
             }
 
-            
+
 
             //This section presents the future to the user
-            Console.WriteLine("********************************************************************************\n");
-
-
+            Console.Write("*******************************************************************************\n\n");
+            Console.Write("  Madame Zelga has looked into your future and she has seen your fortune\n\n");
+            Console.Write("*  {0} {1} will retire in {2} years with {3} in the bank,a vacation home in\n\n", firstUpper, lastUpper, yearRetire, bankAcctFuture);
+            Console.Write("*                            {0} with a {1}.\n\n", vacationHome, autoChoice);
+            Console.Write("*******************************************************************************\n\n");
 
 
 
